091271a1bc0eccc1aca4e5dae9ece83357822326
Gurpreet1927 <singhgurpreet2725@gmail.com>
2025-09-20 12:24:21 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from '../context/AuthContext.jsx'
 import { ChatProvider } from '../context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/chat">
+  <BrowserRouter basename="/Chatapp">
     <AuthProvider>
       <ChatProvider>
          <App/>
